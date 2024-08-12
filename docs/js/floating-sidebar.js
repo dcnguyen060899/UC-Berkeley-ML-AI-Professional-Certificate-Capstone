beaafbd97d2b6cb07bb6f694c02fcef16d83f739
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-08-12 13:37:31 -0700

    Update floating-sidebar.js

--- docs/js/floating-sidebar.js
+++ docs/js/floating-sidebar.js
@@ -7,6 +7,9 @@ var btn = document.getElementById("certificate-button");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Get the image inside the modal
+var img = document.getElementById("certificate-image");
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -17,9 +20,14 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the image, close the modal
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || event.target == span) {
         modal.style.display = "none";
     }
+}
+
+// Prevent the modal from closing when clicking on the image
+img.onclick = function(event) {
+    event.stopPropagation();
 }
