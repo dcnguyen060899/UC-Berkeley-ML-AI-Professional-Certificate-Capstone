5713b4aa6a013a43ce20f1a3f69e4bf88d75978b
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2025-03-01 17:12:12 -0800

    Update chatservice.py

--- backend/src/chatservice.py
+++ backend/src/chatservice.py
@@ -1,22 +1,21 @@
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import OpenAI
-from agent import generate_response
-from agent import generate_evaluation_response
+from agent import generate_response, generate_evaluation_response
 import os
 
-class ChatService:e
+class ChatService:
     def __init__(self, api_key):
         self.api_key = api_key
         self.template = """Question: {question}
 
-Answer: Let's think step by step."""
+        Answer: Let's think step by step."""
         self.prompt = PromptTemplate(template=self.template, input_variables=["question"])
+    def get_evaluation_response(self, user_message):
+        response = generate_evaluation_response(user_message)
+        return response
 
     def get_response(self, user_message):
         response = generate_response(user_message)
         return response
-    
-    def get_evaluation_response(self, user_message):
-        response = generate_evaluation_response(user_message)
-        return response
+
